15a88aae1c297e8be29382cafe20bd7935c868de
Dominique <dc@webformation.fr>
2019-05-13 14:32:58 +0200

    ajout Exception

---
+++ Exception/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExempleException
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int d;
+                int.TryParse(args[0], out d);
+                int i = 1 / d;
+                return;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+            finally
+            {
+                Console.WriteLine("Code exécuté dans tous les cas");
+            }
+        }
+    }
+}
