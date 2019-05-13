7103fac2dc4ae96427fb990d46ea2699e3a0ba7a
Dominique <dc@webformation.fr>
2019-05-13 16:17:06 +0200

    ajout Héritage

---
+++ Heritage/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Heritage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Ami a = new Ami("Jean", "Dupont");
+            a.Tel();
+            a.Mail();
+            Personne p = a;
+            p.Mail();
+            p.SMS();
+            p.Skype();
+        }
+    }
+    public abstract class Personne
+    {
+        public string nom;
+        public Personne(string nom)
+        {
+            this.nom = nom;
+        }
+        public void Tel()
+        {
+            Console.WriteLine("Téléphoner à " + nom);
+        }
+        public void Mail()
+        {
+            Console.WriteLine("Envoyer un mail à " + nom);
+        }
+        public virtual void SMS()
+        {
+            Console.WriteLine("Envoyer un sms à " + nom);
+        }
+        public abstract void Skype();
+    }
+    public class Ami : Personne
+    {
+        public string prenom;
+        public Ami(string prenom, string nom) : base(nom)
+        {            
+            this.prenom = prenom;
+        }
+        public void Mail()
+        {
+            Console.WriteLine("Envoyer un mail à " + prenom);
+        }
+        public override  void SMS()
+        {
+            Console.WriteLine("Envoyer un sms à " + prenom);
+        }
+        public override void Skype()
+        {
+            Console.WriteLine("Lancer Skype avec " + prenom);
+        }
+    }
+}
+
+
