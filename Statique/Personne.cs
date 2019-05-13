0805010a70ae7f3f38f774bb51321a3ec4b07f2a
Dominique <dc@webformation.fr>
2019-05-13 10:49:17 +0200

    ajout Statique

---
+++ Statique/Personne.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Statique
+{
+    class Personne : IDisposable
+    {
+        public string nom;
+        public int age;
+        static int nombre = 0;
+        public Personne(string nom, int age)
+        {
+            this.nom = nom;
+            this.age = age;
+            ++nombre;
+        }
+        public void Dispose()
+        {
+            --nombre;
+        }
+        public static int nb
+        {
+            get
+            {
+                return nombre;
+            }
+        }
+    }
+}
---
+++ Statique/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Statique
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Nombre de personnes : {0}", Personne.nb);
+            Personne p1 = new Personne("Jean", 20);
+            Console.WriteLine("Nombre de personnes : {0}", Personne.nb);
+            using (Personne p2 = new Personne("Marc", 30))
+            {
+                Console.WriteLine("Nombre de personnes : {0}", Personne.nb);
+            }
+            Console.WriteLine("Nombre de personnes : {0}", Personne.nb);
+        }
+    }
+}
