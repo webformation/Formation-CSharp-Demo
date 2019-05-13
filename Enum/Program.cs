512f831e7f0669065d3cb59f2ca6202df2c02d9d
Dominique <dc@webformation.fr>
2019-05-13 14:58:00 +0200

    Ajout Enum

---
+++ Enum/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExempleEnum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            jour j = jour.lundi;
+            Console.WriteLine(j);
+        }
+    }
+    enum jour { lundi,mardi,mercredi, jeudi, vendredi, samed,dimanche}
+}
