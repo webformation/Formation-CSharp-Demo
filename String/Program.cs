249b4e83026d74be23043dc5096a0f3f1b8bd2d2
Dominique <dc@webformation.fr>
2019-05-13 09:30:03 +0200

    ajout string

---
+++ String/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "Bonjour";
+            s += " à tous";
+            string s1 = s.Insert(7, " à toutes et");
+            Console.WriteLine(s);
+            Console.WriteLine(s1);
+            if (s1.Contains("toutes")) Console.WriteLine("Féminin");
+            Console.WriteLine(s1.ToUpper());
+        }
+    }
+}
