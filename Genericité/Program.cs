7a54810a8cbbb9bc5202bb05bc68916337598a2d
Dominique <dc@webformation.fr>
2019-05-13 09:56:54 +0200

    Ajout Genericite

---
+++ Genericité/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ExempleGenericité
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            mesure<int> m1 = new mesure<int>();
+            m1.valeur = 3;
+            Console.WriteLine(m1.valeur);
+            mesure<double>[] banc1 = new mesure<double>[20];
+            banc1[0] = new mesure<double>(3.14);
+            Console.WriteLine(banc1[0].valeur);
+        }
+    }
+    class mesure<T>
+    {
+        private T v;
+        public T valeur
+        {
+            get { return v; }
+            set
+            {
+                if (value != null)
+                {
+                    v = value;
+                }
+            }
+        }
+        public mesure() { }
+        public mesure (T x) { valeur = x; }
+    }
+}
