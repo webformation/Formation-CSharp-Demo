484b1cd479544b5805c7e6545dc21fffe5890c3c
colombani <dc@webformation.fr>
2019-05-27 17:58:51 +0100

    ajout Indexeur

---
+++ Indexeur/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ExempleIndexeur
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Bibliotheque b1 = new Bibliotheque();
+            b1[0] = "Le C# c'est simple";
+            b1[1] = "Utiliser le Framework.NET";
+            Console.WriteLine(b1[1]);
+            Console.WriteLine(b1[0]);
+            Console.ReadKey();
+        }
+    }
+    class Bibliotheque
+    {
+        string[] _livres;
+        public Bibliotheque()
+        {
+            _livres = new string[10];
+        }
+        public string this[int index]
+        {
+            get { return _livres[index]; }
+            set { _livres[index] = value; }
+        }
+    }
+}
