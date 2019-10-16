bf4e7a16a7ea0d4159e28fe65dff0e3fddbb4b89
colombani <dc@webformation.fr>
2019-10-16 17:56:05 +0100

    ajout liste des membres d'une énumération

--- Enum/Program.cs
+++ Enum/Program.cs
@@ -6,13 +6,19 @@ using System.Threading.Tasks;
 
 namespace ExempleEnum
 {
-class Program
-{
-    static void Main(string[] args)
+    class Program
     {
-        jour j = jour.lundi;
-        Console.WriteLine(j);
+        static void Main(string[] args)
+        {
+            jour j = jour.lundi;
+            Console.WriteLine(j);
+            foreach (var x in Enum.GetValues(typeof(jour)))
+            {
+                Console.Write(x + " ");
+            }
+            Console.WriteLine();
+            Console.ReadKey();
+        }
     }
-}
-enum jour { lundi,mardi,mercredi, jeudi, vendredi, samed,dimanche}
+    enum jour { lundi, mardi, mercredi, jeudi, vendredi, samed, dimanche }
 }
